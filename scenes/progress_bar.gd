26d0fe7c2199fc0f5a0a760ea84a4e1fc18ebf43
GlaggleGuy <mylesanathan@gmail.com>
2025-08-27 15:32:46 -0400

    sprint fix

--- scenes/progress_bar.gd
+++ scenes/progress_bar.gd
@@ -3,5 +3,5 @@ extends ProgressBar
 @onready var player: CharacterBody2D = $"../.."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	value = player.stamina
